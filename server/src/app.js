4070797870fe3e816d9beeed20f4697eaa56488a
Nirup Murali <nirupg63@gmail.com>
2022-01-11 00:32:28 +0530

    added api root to server

--- server/src/app.js
+++ server/src/app.js
@@ -7,7 +7,7 @@ const port = 9000
 
 app.use(cors());
 
-app.get('/word', async (req, res) => {
+app.get('/api/word', async (req, res) => {
     await new Promise(resolve => setTimeout(resolve, 3000));
     res.send({word: randomWords()})
 })
