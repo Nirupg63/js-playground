ebb9f4b27367c0c6857002f6a74a392d6c614336
Nirup Murali <nirupg63@gmail.com>
2022-01-22 13:26:09 +0530

    added workflow

---
+++ build.bat
@@ -0,0 +1,5 @@
+cd server
+call npm install
+cd ../ui
+call npm install
+call npm run build
--- server/server.js
+++ server/server.js
@@ -13,5 +13,5 @@ app.get('/api/word', async (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at PORT:${port}`)
 })
